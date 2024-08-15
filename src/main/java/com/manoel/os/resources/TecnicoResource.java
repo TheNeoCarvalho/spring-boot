b381709a29d8bf20e4a87c04096f7923c5b00f90
Manoel Carvalho <manoelmecat@gmail.com>
2024-08-15 14:52:01 -0300

    feat: add tecnico

--- src/main/java/com/manoel/os/resources/TecnicoResource.java
+++ src/main/java/com/manoel/os/resources/TecnicoResource.java
@@ -1,11 +1,18 @@
 package com.manoel.os.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.manoel.os.domain.Tecnico;
 import com.manoel.os.dtos.TecnicoDTO;
@@ -24,4 +31,19 @@ public class TecnicoResource {
 		return ResponseEntity.ok().body(objDTO);
 	}
 
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<TecnicoDTO> listDTO = service.findAll().stream().map(obj -> new TecnicoDTO(obj))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO) {
+		Tecnico newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
 }
--- src/main/java/com/manoel/os/services/TecnicoService.java
+++ src/main/java/com/manoel/os/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.manoel.os.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.manoel.os.domain.Tecnico;
+import com.manoel.os.dtos.TecnicoDTO;
 import com.manoel.os.repositories.TecnicoRepository;
 import com.manoel.os.services.exceptions.ObjectNotFoundException;
 
@@ -14,13 +16,23 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	public Tecnico findById(Integer id) {
-		
+
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado id: "+ id + ", tipo: " + Tecnico.class.getName()
-				));
-		
+				"Objeto não encontrado id: " + id + ", tipo: " + Tecnico.class.getName()));
+
+	}
+
+	public List<Tecnico> findAll() {
+		List<Tecnico> obj = repository.findAll();
+		return obj;
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		Tecnico newObj = new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone());
+		return repository.save(newObj);
+
 	}
 }
